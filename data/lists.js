1afe26f1585838f7e6680b30697eed32e5d602bb
Rajasegar <rajasegar.c@gmail.com>
2021-03-31 16:29:51 +0530

    chore: add endpoints for adding cards and lists

--- data/lists.js
+++ data/lists.js
@@ -1,23 +1,29 @@
 module.exports = [
   {
     name: 'First List',
+    id: 1,
     cards: [
       { 
-        label: 'First Card'
+        label: 'First Card',
+        list: 1
       },
       { 
-        label: 'Second Card'
+        label: 'Second Card',
+        list: 1
       },
     ]
   },
   {
     name: 'Second List',
+    id: 2,
     cards: [
       { 
-        label: 'First Card'
+        label: 'First Card',
+        list: 2,
       },
       { 
-        label: 'Second Card'
+        label: 'Second Card',
+        list: 2
       },
     ]
   },
--- index.js
+++ index.js
@@ -17,18 +17,52 @@ app.get('/', (req, res) => {
   res.render('index', { lists });
 });
 
-app.get('/cards/add', (req, res) => {
+app.get('/cards/add/:id', (req, res) => {
+  const { id } = req.params;
   const template = pug.compileFile('views/_add-card.pug');
-  const markup = template();
+  const markup = template({ id });
   res.send(markup);
 });
 
-app.get('/cards/cancel', (req, res) => {
+app.get('/cards/cancel/:id', (req, res) => {
+  const { id } = req.params;
+  const list = lists.find(l => l.id == id);
   const template = pug.compileFile('views/_toggle-add-card.pug');
-  const markup = template();
+  const markup = template({ list });
   res.send(markup);
 });
 
+app.post('/cards', (req,res) => {
+  console.log(req.body);
+  const { label, listId } = req.body;
+  const list = lists.find(l => l.id == listId);
+  const card = {
+    label
+  };
+  const template = pug.compileFile('views/_card.pug');
+  const markup = template({ card, list } );
+  res.send(markup);
+});
+
+app.get('/lists/add', (req, res) => {
+  const template = pug.compileFile('views/_add-list.pug');
+  const markup = template({  });
+  res.send(markup);
+});
+
+app.post('/lists', (req,res) => {
+  const { name } = req.body;
+  const newList = {
+    name,
+    id: lists.length + 1,
+    cards: []
+  };
+  lists.push(newList);
+  const template = pug.compileFile('views/_list.pug');
+  const markup = template({ list: newList } );
+  res.send(markup);
+
+});
 app.listen(PORT);
 
 console.log('Listening on port: ', PORT);
